4328d91bbc61e217e499be6ec5fd8fb394ebebc4
Ethan Hubbartt <33075924+ehubbartt@users.noreply.github.com>
2025-11-29 23:07:35 -0500

    debug testing

--- src/test/java/com/catracker/CombatAchievementsPluginTest.java
+++ src/test/java/com/catracker/CombatAchievementsPluginTest.java
@@ -8,9 +8,18 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
  * This allows running RuneLite with the plugin loaded directly
  */
 public class CombatAchievementsPluginTest {
-    public static void main(String[] args) throws Exception {
-        ExternalPluginManager.loadBuiltin(CombatAchievementsPlugin.class);
+	public static void main(String[] args) throws Exception {
+		// Enable assertions (equivalent to -ea VM argument)
+		ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
 
-        RuneLite.main(args);
-    }
+		ExternalPluginManager.loadBuiltin(CombatAchievementsPlugin.class);
+
+		// Add developer mode and debug arguments
+		String[] newArgs = new String[args.length + 2];
+		System.arraycopy(args, 0, newArgs, 0, args.length);
+		newArgs[args.length] = "--developer-mode";
+		newArgs[args.length + 1] = "--debug";
+
+		RuneLite.main(newArgs);
+	}
 }
